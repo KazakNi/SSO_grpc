87da5620ceee47585cd49db9f12e648ba5acb8e6
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-01-31 20:42:18 +0300

    feat: adjusted dependencies

--- sso/internal/config/config.go
+++ sso/internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
 	Env         string        `yaml:"env" env-default:"local"`
@@ -15,5 +21,30 @@ type GRPCConfig struct {
 }
 
 func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
 
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist" + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config" + err.Error())
+	}
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+	return res
 }
